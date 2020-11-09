37f3d0e008b47fa99fbe633101f1c2a8ce0b9dbc
ren0715 <71118115+ren0715@users.noreply.github.com>
2020-11-09 17:45:29 +0900

    [Function] Logout (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources:users
   root 'pages#home'
   get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
