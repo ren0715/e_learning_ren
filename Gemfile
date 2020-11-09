9a1c321362bd9c6eab1466702650fd7d72d2af6b
ren0715 <r.s.izxo715@gmail.com>
2020-11-09 16:22:23 +0900

    [Layout] Create Sign up

--- Gemfile
+++ Gemfile
@@ -76,3 +76,7 @@ gem 'will_paginate-bootstrap4'
 gem 'carrierwave', '1.2.2'
 
 gem 'mini_magick', '4.7.0'
+
+gem 'yaml_db'
+
+gem 'mysql2', '>= 0.4.4', '< 0.6.0'
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
