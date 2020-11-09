db97be544a957ec8153b6a548ae484145737ac6b
ren0715 <71118115+ren0715@users.noreply.github.com>
2020-11-09 17:25:45 +0900

    [Function] Add Login (#5)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def logged_in?              #return true if current/user is not nil
+    !current_user.nil?        #check if current_user is not nill
+  end
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
