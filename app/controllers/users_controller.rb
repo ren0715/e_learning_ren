d459b5bd0e3cb6c821f7f6683603bc5bcaad6128
ren0715 <r.s.izxo715@gmail.com>
2020-11-09 16:44:03 +0900

    [Function] Add Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:info] = "Please log in."
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
   def show
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
