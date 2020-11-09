52cb4d8076e0229da9b613c71c20a202e2453f58
ren0715 <r.s.izxo715@gmail.com>
2020-11-09 17:21:39 +0900

    [Function] Add Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+
+    # check is @user is not empty
+    # check if @user's password is equal to params[:session][:password]
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      flash[:info] = "Successfully logged in."
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Invalid Credentials!"
+      render 'new'
+    end
   end
 end
