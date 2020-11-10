a0920d40ee9e6c4eea4c12859cc810f69b8ad693
ren0715 <71118115+ren0715@users.noreply.github.com>
2020-11-10 17:44:19 +0900

    [Function] Add Category (#12)
    * [Function] Add Category
    
    * Simplified

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,24 @@
 class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+  def new
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:success] = "category created!"
+      redirect_to admin_categories_url
+    else
+      render 'admin/new' 
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_093601) do
+ActiveRecord::Schema.define(version: 2020_11_10_074631) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
