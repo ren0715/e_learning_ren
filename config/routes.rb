d4e38e76c399106d92644cf39eeb1cd54eca4ae8
ren0715 <r.s.izxo715@gmail.com>
2020-11-13 18:46:06 +0900

    [Layout] Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  
   namespace :admin do
     resources:users
     resources:categories do
       resources:words
     end
   end
+  resources:categories
   resources:sessions
   resources:users do
     get 'dashboard'
   end
   root 'pages#home'
+  get 'categories/index'
   get '/login', to: 'sessions#new'
   delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
