434bf0e45884e1465271bcf692ab392fc78a03ab
ren0715 <r.s.izxo715@gmail.com>
2020-11-13 17:58:29 +0900

    Fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "Ren Shimada",
+  email: "r.s.izxo715@gmail.com",
+  password: "password",
+  is_admin: true
+)
+
+50.times do |n|
+  User.create!(
+    name: Faker::Movies::HarryPotter.character,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    is_admin: false
+  )
+end
+
+users = User.order(:created_at).take(6)
