4510ecb2b2f4ef50ef6f79852da04e557bfce42e
ren0715 <r.s.izxo715@gmail.com>
2020-11-20 17:47:14 +0900

    [Function] Category Filter

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,13 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all.paginate(page: params[:page], per_page: 10)
+    
+    @ids = current_user.lessons.pluck(:category_id)
+    if params[:learned].eql?("learned")
+      @categories = Category.where(id: @ids).paginate(page: params[:page], per_page: 10)
+    elsif params[:learned].eql?("notlearned")
+      @categories = Category.where.not(id: @ids).paginate(page: params[:page], per_page: 10)
+    else
+      @categories = Category.all.paginate(page: params[:page], per_page: 10)
+    end
   end
 end
