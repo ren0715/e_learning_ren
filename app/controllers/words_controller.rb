dca88f0f1d587347fa73643bb50f99ca73834e68
ren0715 <r.s.izxo715@gmail.com>
2020-11-20 18:46:36 +0900

    [Function]Word Filter

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,8 +1,15 @@
 class WordsController < ApplicationController
   def index
-    @lessons = Lesson.all.where(user_id: current_user.id)
+    @cate = params[:cate]
+    @category = Category.find_by(title: params[:cate])
+    if @cate
+      @lessons = Lesson.where(category_id: @category.id)
+    else
+      @lessons = Lesson.where(user_id: current_user.id)
+    end
+    
     # @answers = @lessons.answers
-    # @categories = Category.all.where(id: @lessons.category_id)
+    @categories = Category.all
     # @words = @categories.words
     
   end
