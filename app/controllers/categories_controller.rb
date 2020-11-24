ecf7066c807ffb36ced45f68ec4f4f2da1acaa1f
ren0715 <71118115+ren0715@users.noreply.github.com>
2020-11-24 17:49:59 +0900

    [Function] Zero (#40)
    * [Function] Zero
    
    * Fixed
    
    * Fixed2
    
    * Fixed3

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,11 +4,11 @@ class CategoriesController < ApplicationController
     @page = params[:learned]
     @ids = current_user.lessons.pluck(:category_id)
     if params[:learned].eql?("learned")
-      @categories = Category.where(id: @ids).paginate(page: params[:page], per_page: 10)
+      @categories = Category.joins(:words).distinct.where(id: @ids).paginate(page: params[:page], per_page: 10)
     elsif params[:learned].eql?("notlearned")
-      @categories = Category.where.not(id: @ids).paginate(page: params[:page], per_page: 10)
+      @categories = Category.joins(:words).distinct.where.not(id: @ids).paginate(page: params[:page], per_page: 10)
     else
-      @categories = Category.all.paginate(page: params[:page], per_page: 10)
+      @categories = Category.joins(:words).distinct.paginate(page: params[:page], per_page: 10)
     end
   end
   
