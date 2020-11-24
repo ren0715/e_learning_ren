a0ad2f919b336a9de638bf5958559791d88214a8
ren0715 <r.s.izxo715@gmail.com>
2020-11-24 17:48:24 +0900

    Fixed3

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,18 +4,12 @@ class CategoriesController < ApplicationController
     @page = params[:learned]
     @ids = current_user.lessons.pluck(:category_id)
     if params[:learned].eql?("learned")
-      @categories = Category.where(id: @ids).joins("INNER JOIN words ON words.category_id = categories.id").distinct.paginate(page: params[:page], per_page: 10)
+      @categories = Category.joins(:words).distinct.where(id: @ids).paginate(page: params[:page], per_page: 10)
     elsif params[:learned].eql?("notlearned")
-      @categories = Category.where.not(id: @ids).joins("INNER JOIN words ON words.category_id = categories.id").distinct.paginate(page: params[:page], per_page: 10)
+      @categories = Category.joins(:words).distinct.where.not(id: @ids).paginate(page: params[:page], per_page: 10)
     else
-      @categories = Category.all.joins("INNER JOIN words ON words.category_id = categories.id").distinct.paginate(page: params[:page], per_page: 10)
+      @categories = Category.joins(:words).distinct.paginate(page: params[:page], per_page: 10)
     end
-    # @count = 0
-    # @categories.each do |category|
-    #   if category.words.count != 0
-    #     @count +=1
-    #   end
-    # end
   end
   
 end
