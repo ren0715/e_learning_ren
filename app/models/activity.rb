4618d850bbfc482bf3623ed6bdcba2537d5f1106
ren0715 <71118115+ren0715@users.noreply.github.com>
2020-11-24 16:38:07 +0900

    [Function] Vote (#39)
    * [Function] Vote
    
    * Fixed
    
    * Fixed2

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,5 @@ class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :action, polymorphic: true
   default_scope { order(created_at: :desc) }
+  has_many :votes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_080440) do
+ActiveRecord::Schema.define(version: 2020_11_24_063339) do
 
   create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -69,6 +69,13 @@ ActiveRecord::Schema.define(version: 2020_11_19_080440) do
     t.string "password_digest"
     t.string "avatar"
     t.boolean "is_admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "activity_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
